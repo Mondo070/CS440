dbdb58f46841a74f78e5c462b04f11a49b2a0919
2310hs <103340928+2310hs@users.noreply.github.com>
2022-09-24 01:01:36 -0400

    update Main.java
    added Scanner in order to read the text file in the format given in the assignment and applying it to the grid

--- src/com/ai/Main.java
+++ src/com/ai/Main.java
@@ -1,13 +1,37 @@
 package com.ai;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
-        int[][] grid = new int[4][3];
-        grid[0][1] = 1;
-        grid[1][1] = 1;
-        grid[3][1] = 1;
+    	File file = new File("C:\\Users\\Hunter\\eclipse-workspace\\Assignment 1\\src\\grid1");
+        Scanner sc = new Scanner(file);
+        int gridRow;
+        int gridCol;
+        int startRow;
+        int startCol;
+        int goalRow;
+        int goalCol;
+        int currRow;
+        int currCol;
+        int[][] grid = null;
+        while(sc.hasNextLine()) {
+        	startRow = sc.nextInt();
+        	startCol = sc.nextInt();
+        	goalRow = sc.nextInt();
+        	goalCol = sc.nextInt();
+        	gridRow = sc.nextInt();
+        	gridCol = sc.nextInt();
+        	grid = new int[gridRow][gridCol];
+        	while(sc.hasNextInt()) {
+        		currRow = sc.nextInt();
+        		currCol = sc.nextInt();
+        		grid[currRow-1][currCol-1] = sc.nextInt();
+        	}
+        }
 
         for (int i = 0; i < grid[0].length + 1; i++) {
             for (int j = 0; j < grid.length + 1; j++) {
