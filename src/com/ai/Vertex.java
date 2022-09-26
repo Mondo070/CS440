b02843f41d33696adc3287e8d663972d8fdcb0b9
2310hs <103340928+2310hs@users.noreply.github.com>
2022-09-26 18:59:03 -0400

    Update Vertex.java

--- src/com/ai/Vertex.java
+++ src/com/ai/Vertex.java
@@ -2,9 +2,29 @@ package com.ai;
 
 public class Vertex {
 
-    private float f;
-    private float g;
-    private float h;
-
+	Vertex parent;
+    int row;
+    int col;
+    boolean start;
+    boolean goal;
+    boolean open;
+    
+	public Vertex(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	public void setStart() {
+		start = true;
+	}
+	public void setGoal() {
+		goal = true;
+	}
+	public int getRow() {
+		return row;
+	}
+	public int getCol() {
+		return col;
+	}
+	
 
 }
