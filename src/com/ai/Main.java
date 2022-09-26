53874b48548e0d90e25d26b980a6e91f9c923dee
2310hs <103340928+2310hs@users.noreply.github.com>
2022-09-26 18:58:35 -0400

    Update Main.java

--- src/com/ai/Main.java
+++ src/com/ai/Main.java
@@ -4,13 +4,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Main {
-
+	
     public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
     	File file = new File("C:\\Users\\Hunter\\eclipse-workspace\\Assignment 1\\src\\grid1");
         Scanner sc = new Scanner(file);
-        int gridRow;
-        int gridCol;
+        int gridRow = 0;
+        int gridCol = 0;
         int startRow = 0;
         int startCol = 0;
         int goalRow = 0;
@@ -18,6 +18,8 @@ public class Main {
         int currRow;
         int currCol;
         int[][] grid = null;
+        Vertex sVertex, gVertex, cVertex;
+        
         while(sc.hasNextLine()) {
         	startCol = sc.nextInt();
         	startRow = sc.nextInt();
@@ -31,8 +33,8 @@ public class Main {
         		currCol = sc.nextInt();
         		grid[currRow-1][currCol-1] = sc.nextInt();
         	}
+        	
         }
-
         for (int i = 0; i < grid[0].length + 1; i++) {
             for (int j = 0; j < grid.length + 1; j++) {
             	if (i == startRow-1 && j == startCol-1) {
@@ -55,5 +57,19 @@ public class Main {
             }
             System.out.println();
         }
+        
+        Vertex[][] vertices = new Vertex[gridRow+1][gridCol+1];
+        for (int i = 0; i < vertices.length; i++) {
+        	for (int j = 0; j < vertices[0].length; j++) {
+        		vertices[i][j] = new Vertex(i,j);
+        	}	
+        }
+        sVertex = vertices[startRow-1][startCol-1];
+        cVertex = sVertex;
+        gVertex = vertices[goalRow-1][goalCol-1];
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Enter coordinates to display desired vertex info (enter the row then column):");
+        int inputRow = sc2.nextInt();
+        int inputCol = sc2.nextInt();
     }
 }
