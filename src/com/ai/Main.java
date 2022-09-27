858dc602eae24ccfab0994fd93d85f809ba19f9a
Mondo070 <amerino0530@gmail.com>
2022-09-27 04:34:08 -0400

    basic path finding

--- src/com/ai/Main.java
+++ src/com/ai/Main.java
@@ -1,4 +1,6 @@
 package com.ai;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,14 +9,34 @@ public class Main {
 	
     public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
-    	File file = new File("C:\\Users\\Hunter\\eclipse-workspace\\Assignment 1\\src\\grid1");
+        int[][] map = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 1},
+                {1, 0, 0, 1, 1},
+                {0, 0, 0, 1, 0},
+                {1, 1, 0, 0, 1}
+        };
+
+        Vertex start = new Vertex(0, 0, null);
+        Vertex end = new Vertex(3, 4, null);
+        List<Vertex> path = FindPath(map, start, end);
+        if (path != null) {
+            for (Vertex v : path) {
+                System.out.println(v);
+            }
+        }
+        else
+            System.out.println("No path found");
+    }
+
+    	/*File file = new File("C:\\Users\\Hunter\\eclipse-workspace\\Assignment 1\\src\\grid1");
         Scanner sc = new Scanner(file);
-        int gridRow = 0;
-        int gridCol = 0;
-        int startRow = 0;
-        int startCol = 0;
-        int goalRow = 0;
-        int goalCol = 0;
+        int gridRow;
+        int gridCol;
+        int startRow;
+        int startCol;
+        int goalRow;
+        int goalCol;
         int currRow;
         int currCol;
         int[][] grid = null;
@@ -71,5 +93,61 @@ public class Main {
         System.out.println("Enter coordinates to display desired vertex info (enter the row then column):");
         int inputRow = sc2.nextInt();
         int inputCol = sc2.nextInt();
+    }*/
+
+    public static boolean IsValidMove(int[][] grid, Vertex v) {
+        if (v.row < 0 || v.row > grid.length - 1) return false;
+        if (v.col < 0 || v.col > grid[0].length - 1) return false;
+        return grid[v.col][v.row] == 0;
+    }
+
+    public static List<Vertex> FindNeighbors(int[][] grid, Vertex v) {
+        List<Vertex> neighbors = new ArrayList<>();
+        Vertex toLeft = v.offset(-1, 0);
+        Vertex toRight = v.offset(1, 0);
+        Vertex toAbove = v.offset(0, 1);
+        Vertex toBelow = v.offset(0 , -1);
+        if (IsValidMove(grid, toLeft)) neighbors.add(toLeft);
+        if (IsValidMove(grid, toRight)) neighbors.add(toRight);
+        if (IsValidMove(grid, toAbove)) neighbors.add(toAbove);
+        if (IsValidMove(grid, toBelow)) neighbors.add(toBelow);
+
+        return neighbors;
+    }
+
+    public static List<Vertex> FindPath(int[][] map, Vertex start, Vertex end) {
+        boolean finished = false;
+        List<Vertex> visited = new ArrayList<>();
+        visited.add(start);
+
+        while (!finished) {
+            List<Vertex> newOpen = new ArrayList<>();
+            for (Vertex v: visited) {
+                for (Vertex neighbor : FindNeighbors(map, v)) {
+                    if (!visited.contains(neighbor) && !newOpen.contains(neighbor)) {
+                        newOpen.add(neighbor);
+                    }
+                }
+            }
+
+            for(Vertex v: newOpen) {
+                visited.add(v);
+                if (end.equals(v)) {
+                    finished = true;
+                    break;
+                }
+            }
+
+            if (!finished && newOpen.isEmpty())
+                return null;
+        }
+
+        List<Vertex> path = new ArrayList<>();
+        Vertex v = visited.get(visited.size() - 1);
+        while(v.parent != null) {
+            path.add(0, v);
+            v = v.parent;
+        }
+        return path;
     }
 }
--- src/com/ai/Vertex.java
+++ src/com/ai/Vertex.java
@@ -5,26 +5,39 @@ public class Vertex {
 	Vertex parent;
     int row;
     int col;
-    boolean start;
-    boolean goal;
-    boolean open;
+//    boolean start;
+//    boolean goal;
+//    boolean open;
     
-	public Vertex(int row, int col) {
+	public Vertex(int row, int col, Vertex parent) {
 		this.row = row;
 		this.col = col;
+        this.parent = parent;
 	}
-	public void setStart() {
+/*	public void setStart() {
 		start = true;
 	}
 	public void setGoal() {
 		goal = true;
-	}
+	}*/
 	public int getRow() {
 		return row;
 	}
 	public int getCol() {
 		return col;
 	}
-	
+
+    @Override
+    public boolean equals(Object o) {
+        Vertex v = (Vertex) o;
+        return row == v.row && col == v.col;
+    }
+
+    public Vertex offset(int offsetX, int offsetY) {
+        return new Vertex(row + offsetX, col + offsetY, this);
+    }
+
+    @Override
+    public String toString() { return String.format("(%d, %d)", row, col); }
 
 }
