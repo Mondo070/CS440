2b6e6f419eb036f59a6eea39be48cad277a8f97b
2310hs <103340928+2310hs@users.noreply.github.com>
2022-09-28 23:05:51 -0400

    Update Logic.java

--- src/com/ai/Logic.java
+++ src/com/ai/Logic.java
@@ -71,10 +71,11 @@ public class Logic {
         v.f = v.g + v.h;
     }
 
-    private static boolean IsValidMove(int[][] grid, Vertex v) {
-        if (v.row < 0 || v.row > grid[0].length) return false;
-        if (v.col < 0 || v.col > grid.length) return false;
+    private static boolean IsValidMove(int[][] grid, Vertex v, Vertex v1) {
+        if (v1.row < 0 || v1.row > grid[0].length) return false;
+        if (v1.col < 0 || v1.col > grid.length) return false;
         //return grid[v.col][v.row] == 0;
+        
         return true;
     }
 
@@ -105,14 +106,14 @@ public class Logic {
         Vertex toSouth = v.offset(0 , -1);
         Vertex toSouthEast = v.offset(1, -1);
 
-        if (IsValidMove(grid, toEast)) neighbors.add(toEast);
-        if (IsValidMove(grid, toNorthEast)) neighbors.add(toNorthEast);
-        if (IsValidMove(grid, toNorth)) neighbors.add(toNorth);
-        if (IsValidMove(grid, toNorthWest)) neighbors.add(toNorthWest);
-        if (IsValidMove(grid, toWest)) neighbors.add(toWest);
-        if (IsValidMove(grid, toSouthWest)) neighbors.add(toSouthWest);
-        if (IsValidMove(grid, toSouth)) neighbors.add(toSouth);
-        if (IsValidMove(grid, toSouthEast)) neighbors.add(toSouthEast);
+        if (IsValidMove(grid, v, toEast)) neighbors.add(toEast);
+        if (IsValidMove(grid, v, toNorthEast)) neighbors.add(toNorthEast);
+        if (IsValidMove(grid, v, toNorth)) neighbors.add(toNorth);
+        if (IsValidMove(grid, v, toNorthWest)) neighbors.add(toNorthWest);
+        if (IsValidMove(grid, v, toWest)) neighbors.add(toWest);
+        if (IsValidMove(grid, v, toSouthWest)) neighbors.add(toSouthWest);
+        if (IsValidMove(grid, v, toSouth)) neighbors.add(toSouth);
+        if (IsValidMove(grid, v, toSouthEast)) neighbors.add(toSouthEast);
 
         return neighbors;
     }
@@ -137,6 +138,92 @@ public class Logic {
             System.out.println(neighbor.f);
             fringe.add(neighbor);
         }
+    }
+    private void UpdateVertexTheta(int grid[][], Vertex s, Vertex neighbor, Vertex goal) {
+    	if (LineOfSight(grid,s.parent,neighbor)) {
+    		if (s.parent.f < neighbor.g) {
+    			neighbor.parent = s.parent;
+    			calcG(s.parent);
+    			calcH(s.parent, goal);
+    			fringe.remove(neighbor);
+    			neighbor.f = neighbor.g + neighbor.h;
+    			fringe.add(neighbor);
+    		}
+    	}
+    	else {
+    		if (s.f < neighbor.g) {
+    			neighbor.parent = s;
+    			calcG(neighbor);
+    			calcH(neighbor, goal);
+    			fringe.remove(neighbor);
+    			neighbor.f = neighbor.g + neighbor.h;
+    			fringe.add(neighbor);
+    		}
+    	}
+    }
+    
+    public static boolean LineOfSight(int[][] grid, Vertex v0, Vertex v1) {
+    	int x0 = v0.row;
+    	int y0 = v0.col;
+    	int x1 = v1.row;
+    	int y1 = v1.col;
+    	int f = 0;
+    	int dx = x1-x0;
+    	int dy = y1-y0;
+    	int sy,sx;
+    	if (dy < 0) {
+    		dy = -dy;
+    		sy = -1;
+    	}
+    	else {
+    		sy = 1;
+    	}
+    	if (dx < 0) {
+    		dx = -dx;
+    		sx = -1;
+    	}
+    	else {
+    		sx = 1;
+    	}
+    	if (dx >= dy) {
+    		while (x0 != x1) {
+    			f = f + dy;
+    			if (f >= dx) {
+    				if (grid[x0 + ((sx - 1) / 2)][y0 + ((sy -  1) / 2)] == 1) {
+    					return false;
+    				}
+    				y0 = y0 + sy;
+    				f = f - dx;
+    			}
+    			if (f != 0 && grid[x0 + ((sx - 1) / 2)][y0 + ((sy -  1) / 2)] == 1) {
+    				return false;
+    			}
+    			if (dy == 0 && grid[x0 + ((sx - 1) / 2)][y0] == 1 && grid[x0 + ((sx - 1) / 2)][y0 - 1] == 1) {
+    				return false;
+    			}
+    			x0 = x0 + sx;
+    		}
+    	}
+    	else {
+    		while (y0 != y1) {
+    			f = f + dx;
+    			if (f >= dy) {
+    				if (grid[x0 + ((sx - 1) / 2)][y0 + ((sy -  1) / 2)] == 1) {
+    					return false;
+    				}
+    				x0 = x0 + sx;
+    				f = f - dy;
+    			}
+    			if (f != 0 && grid[x0 + ((sx - 1) / 2)][y0 + ((sy -  1) / 2)] == 1) {
+    				return false;
+    			}
+    			if (dy == 0 && grid[x0][y0 + ((sy - 1) / 2)] == 1 && grid[x0 - 1][y0 + ((sy - 1) / 2)] == 1) {
+    				return false;
+    			}
+    			y0 = y0 + sy;
+    		}
+    	}
+    	return true;
     }
 
     private static void calcG(Vertex v) {
